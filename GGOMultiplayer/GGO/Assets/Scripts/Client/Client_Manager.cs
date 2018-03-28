0d732b5cd86716ea603a155cbc333aa2e05478e8
Rernokk <ssmith37@uccs.edu>
2018-03-28 12:19:51 -0600

    Mutliplayer Test Commit.

---
+++ GGOMultiplayer/GGO/Assets/Scripts/Client/Client_Manager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[Serializable]
+public class ClientData : MessageBase{
+  public string ClientName = "";
+}
+
+public class Client_Manager : NetworkBehaviour {
+  NetworkClient client;
+  private void Start()
+  {
+    if (isServer || isLocalPlayer){
+      transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    if (isServer || !isLocalPlayer){
+      transform.Find("Camera").gameObject.SetActive(false);
+      //transform.GetChild(1).GetComponent<AudioListener>().enabled = false;
+    }
+
+    if (!isServer){
+      ClientData data = new ClientData();
+      data.ClientName = "Jim";
+      client.Send(MsgType.Connect, data);
+    }
+  }
+
+  private void Update()
+  {
+    if (!isLocalPlayer)
+      return;
+
+    if (Input.GetKey(KeyCode.A))
+    {
+      transform.position -= transform.right * Time.deltaTime * 3f;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      transform.position += transform.right * Time.deltaTime * 3f;
+    }
+
+    if (Input.GetKeyDown(KeyCode.G)){
+      ClientData msg = new ClientData();
+      client.Send(45, msg);
+    }
+  }
+
+  private void OnDisconnectedFromServer(NetworkDisconnection info)
+  {
+    Destroy(gameObject);
+  }
+}
---
+++ GGOMultiplayer/GGO/Assets/Scripts/Server/Server_Manager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Server_Manager : NetworkBehaviour
+{
+  float uptime = 0;
+  int clientsConnected = 0;
+  int netServerCount = 0;
+  private void Start()
+  {
+    if (!isServer)
+    {
+      Destroy(gameObject);
+      return;
+    }
+
+    if (!File.Exists("Data.dat"))
+    {
+      FileStream f = new FileStream("Data.dat", FileMode.Create);
+      f.Close();
+      ServerSaveData();
+    }
+
+    //Client Connected
+    NetworkServer.RegisterHandler(MsgType.Connect, HandleConnection);
+
+    //Client Disconnected
+    NetworkServer.RegisterHandler(MsgType.Disconnect, HandleDisconnection);
+
+    //Increments netServerCount when clients press the G key (Persistance test)
+    NetworkServer.RegisterHandler(45, CountHandler);
+
+    //Loading Data from File;
+    ServerLoadData();
+  }
+
+  private void Update()
+  {
+    uptime += Time.deltaTime;
+    UpdateDebugPane();
+  }
+
+  void HandleConnection(NetworkMessage msg)
+  {
+    clientsConnected++;
+    ClientData msgName = msg.ReadMessage<ClientData>();
+    print("Connected: " + msgName.ClientName + ", " + msg.conn.connectionId);
+  }
+
+  void HandleDisconnection(NetworkMessage msg)
+  {
+    clientsConnected--;
+    print("Disconnected : " + msg.conn.connectionId);
+    NetworkServer.DestroyPlayersForConnection(msg.conn);
+  }
+
+  void CountHandler(NetworkMessage msg)
+  {
+    netServerCount++;
+    ServerSaveData();
+    print("Recieved Count Inc. From Client");
+  }
+
+  void ServerSaveData()
+  {
+    BinaryFormatter bf = new BinaryFormatter();
+    FileStream stream = new FileStream("Data.dat", FileMode.Create);
+    ServerData dat = new ServerData();
+    dat.netCount = netServerCount;
+    bf.Serialize(stream, dat);
+    stream.Close();
+  }
+
+  void ServerLoadData()
+  {
+    if (!File.Exists("Data.dat"))
+      return;
+
+    FileStream stream = new FileStream("Data.dat", FileMode.Open);
+    BinaryFormatter bf = new BinaryFormatter();
+    ServerData dat = (ServerData)bf.Deserialize(stream);
+    stream.Close();
+    netServerCount = dat.netCount;
+  }
+
+  void UpdateDebugPane()
+  {
+    transform.Find("Canvas/Panel/Debug").GetComponent<Text>().text = "Uptime: " + uptime.ToString() + "\nClients Connected: " + clientsConnected.ToString() + "\nClientClicks " + netServerCount.ToString();
+  }
+}
+
+
+[Serializable]
+class ServerData
+{
+  public int netCount = 0;
+
+  public ServerData()
+  {
+    netCount = 0;
+  }
+}
